ccfcb313884c1665e39942b49a018e32cdfa645a
Andrei Chirigut <andreichirigut07@gmail.com>
2019-01-31 14:08:53 +0100

    Atributos de la clase EmpresaAlquiler

--- src/empresa/EmpresaAlquiler.java
+++ src/empresa/EmpresaAlquiler.java
@@ -10,5 +10,27 @@ package empresa;
  * @author andrei
  */
 public class EmpresaAlquiler {
+    // Atributos de la empresa
+
+    private String cif;
+    private String nombre;
+    private String paginaWeb;
+    /* Atributos para controlar el total de clientes que tiene la
+empresa y array de almacenamiento para los objetos Cliente */
+    private int totalClientes;
+    private Cliente[] clientes;
+    /* Atributos para controlar el total de vehiculos disponibles en
+alquiler que tiene la empresa y array de almacenamiento para los objetos
+Vehiculo */
+    private int totalVehiculos;
+    private Vehiculo[] vehiculos;
+    /* Atributos para controlar el histórico de alquileres: total de
+alquileres realizados y array de almacenamiento para los objetos
+
+8
+VehiculoAlquilado */
+    private int totalAlquileres;
+    private VehiculoAlquiler[] alquileres;
+    
     
 }
