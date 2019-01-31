e247b8a1822eb898b5d1bbcc75a865b138085b61
Andrei Chirigut <andreichirigut07@gmail.com>
2019-01-31 14:28:02 +0100

    Metodos de la clase EmpresaAlquiler

--- src/empresa/EmpresaAlquiler.java
+++ src/empresa/EmpresaAlquiler.java
@@ -31,9 +31,9 @@ public class EmpresaAlquiler {
     private VehiculoAlquilado[] alquileres;
 
     // se omiten los métodos ‘get’ y ‘set’ de la clase
-/* Constructor parametrizado donde se establece que el total de clientes
-será 50, igual que el total de vehiculos disponibles. El histórico de
-vehiculos puede contener hasta 100 elementos */
+    /* Constructor parametrizado donde se establece que el total de clientes
+    será 50, igual que el total de vehiculos disponibles. El histórico de
+    vehiculos puede contener hasta 100 elementos */
     EmpresaAlquiler(String cif, String nombre, String paginaWeb) {
         this.cif = cif;
         this.nombre = nombre;
@@ -49,5 +49,46 @@ vehiculos puede contener hasta 100 elementos */
         this.alquileres = new VehiculoAlquilado[100]; // apuntan a null
 
     }
+
+    /*registrarCliente(Cliente nuevo). Añade un nuevo cliente a la lista de
+    clientes de la empresa. El método registrarCliente(Cliente cliente)
+    almacena un objeto de la clase Cliente en la última posición útil del array
+    clientes, dada por la variable totalClientes y a continuación incrementa
+    la variable totalClientes.*/
+    public void registrarCliente(Cliente nuevo) {
+        this.clientes[this.totalClientes] = nuevo;
+        this.totalClientes++;
+    }
+
+    /*registrarVehiculo(Vehiculo n u e v o ). Añade un vehículo al catálogo
+    de vehículos de la empresa. El método registrarVehiculo(Vehiculo vehiculo)
+    almacena un objeto de la clase Vehiculo en la última posición del array
+    vehiculos, dada por la variable totalVehiculos y a continuación incrementa
+    la variable totalVehiculos.*/
+    public void registrarVehiculo(Vehiculo nuevo) {
+        this.vehiculos[this.totalVehiculos] = nuevo;
+        this.totalVehiculos++;
+    }
+
+    /*imprimirClientes(). Muestra la relación de clientes de la empresa. Este
+    método recorre el array clientes y va mostrando los datos de cada
+    cliente.*/
+    public void imprimirClientes() {
+        System.out.println("NIF cliente\tNombre\n");
+        for (int i = 0; i < this.totalClientes; i++) {
+            System.out.println(clientes[i].toString());
+        }
+    }
+
+    /*imprimirVehiculos(). Muestra el catálogo de vehículos de la empresa,
+    recorriendo el array vehiculos y llamando al método getAtributos() de cada
+    uno .*/
+    public void imprimirVehiculos() {
+        System.out.println("Matricula\tModelo\tColor\tImporte\tDisponible\n ");
+        for (int i = 0; i < this.totalVehiculos; i++) {
+            System.out.println(vehiculos[i].toString());
+        }
+    }
+    
 
 }
